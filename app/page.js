672f9c9757cf7bebc2e3fa4b5944f8e0ffd7852a
Kannan Karthikeyan <kannan060200@hotmail.com>
2024-08-12 22:39:00 +0530

    Create Next App

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <p className="absolute inset-0 flex justify-center items-center">Main Page Content</p>
+    </main>
+  );
+}
