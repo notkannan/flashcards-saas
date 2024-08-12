8b722a018792189fb6ab1dd14fdd452388707f3e
Kannan Karthikeyan <kannan060200@hotmail.com>
2024-08-13 01:50:19 +0530

    Create OpenAI & Stripe API Routes

---
+++ app/api/generate/route.js
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const systemPrompt = `You are a flashcard creator. Your task is to generate concise and effective flashcards based on a given topic or content. Follow these guidelines
+
+1. Give clear and concise questions for the front of the flashcard.
+2. Provide accurate and informative answers for the back of the flashcard.
+3. Ensure that each flashcard focuses on a single content or piece of information.
+4. Use simple language to make flashcards accessible to a wide range of learners.
+5. Include a variety of question types, such as definitions, examples, comparisons, and applications.
+6. Avoid overly complex or ambiguous phrasing in both questions and answers.
+7. When appropriate, use mneumonics or memory aids to help reinforce the learning.
+8. Tailor the difficulty level of the flashcards to user's specific preferences.
+9. If given a body of text, extract the most important and relevant information for the flashcards.
+10. Aim is to create a balanced set of flashcards that covers the topic comprehensively.
+
+Remember, the goal is to facilitate effective learning and retention of information through these flashcards
+
+Return in the following JSON formats
+{
+    "flashcards":[
+        {
+            "front": str,
+            "back": str
+        }
+    ]
+}
+`
+export async function POST(req) {
+    const openai = OpenAI()
+    const data = await req.text()
+
+    const completion = await openai.chat.completion.create({
+        messages:[
+            {role: 'system', content: systemPrompt},
+            {role: 'user', content: data},
+        ],
+        model: 'gpt-4o-mini',
+        response_format: {type: 'json_object'},
+    })
+
+    const flashcard = JSON.parse(completion.choices[0].message.content)
+
+    return NextResponse.json(flashcards.flashcard)
+}
+
--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import getStripe from "@/utils/get-stripe";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-red-600">
       <p className="absolute inset-0 flex justify-center items-center">Main Page Content</p>
     </main>
   );
