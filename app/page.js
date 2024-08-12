7625f21c7d98449a974d4d8b37a58f5888fd8942
Kannan Karthikeyan <kannan060200@hotmail.com>
2024-08-13 02:30:08 +0530

    Create Hero and Navbar

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import getStripe from "@/utils/get-stripe";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Navbar from "./components/Navbar";
+import Hero from './components/Hero';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-red-600">
-      <p className="absolute inset-0 flex justify-center items-center">Main Page Content</p>
-    </main>
+    <div className="w-screen h-screen bg-pink-200">
+      <Hero />
+    </div>
   );
 }
