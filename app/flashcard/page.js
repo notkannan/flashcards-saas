876e3872e3ddddf98eb45d28f228b2f4f57c9c1e
Kannan Karthikeyan <kannan060200@hotmail.com>
2024-08-15 01:42:17 +0530

    Create Flashcard and Flashcards
    Pages to hold all the user's flashcards

---
+++ app/flashcard/page.js
@@ -0,0 +1,14 @@
+'use client'
+
+import { useUser } from "@clerk/nextjs"
+import { useEffect } from "react"
+import { collection, doc, getDoc, getDocs } from "firebase/firestore"
+import { db } from "@/firebase"
+
+import { useSearchParams } from "next/navigation"
+
+export default function Flashcard() {
+    return (
+        
+    )
+}
