8e2ab0e2646e353e20d8f972484140cd614f62c2
Kannan Karthikeyan <kannan060200@hotmail.com>
2024-08-21 02:32:30 +0530

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -30,7 +30,7 @@ const features = [
 
 export default function About() {
   return (
-    <div className="bg-background py-24 sm:py-32">
+    <div className="bg-background py-32 sm:py-20">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="text-base font-semibold leading-7 text-primary">We are Card Flix</h2>
